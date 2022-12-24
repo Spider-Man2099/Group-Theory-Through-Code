5ba59fb905829e5355718dc57fdadea690c1de66
Kenya Mitchell <102193684+Spider-Man2099@users.noreply.github.com>
2022-12-24 20:42:50 +0000

    Clean up comments

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #include <ctime>
 
 //Need to calculate subgroups for certain groups 
+
 /**
 Definition of subgroups: 
 
@@ -13,35 +14,10 @@ A nonempty subset H of a group G is a subgroup of G provided that
 (i) If a, b \in H, then ab \in H
 (ii) If a \in H, then a^{-1} \in H
 
-*/
-//also make one to check cyclic groups? 
-/**
-Function to find centralizer of a group 
-*/
-
-bool findInverse(int sub[]){
-
-  //for(int i = 0; i < sub.length(); i++){
-
-    
- // }
-  
-}
-
-void subgroup(int group[], int sub[]){
-
-
-  
-  
-}
+**/
 
 int main() {
 
-    // Using time point and system_clock
-   // std::chrono::time_point<std::chrono::system_clock> start, end;
-  
-  //  start = std::chrono::system_clock::now();
-  
   int userInput; 
 
   cout << "Enter an integer value: " << endl << endl; 
@@ -66,12 +42,4 @@ int main() {
   
   group->cyclicSub(userInput2); 
   
-   
-  //  end = std::chrono::system_clock::now();
-  
- //   std::chrono::duration<double> elapsed_seconds = end - start;
-   // std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-  
-   // std::cout << "finished computation at " << std::ctime(&end_time)
-      //        << "elapsed time: " << elapsed_seconds.count() << "s\n";
 }
